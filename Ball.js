be7c5d1d45ff806cd278f822b4c69feb52fa7980
Adhyatma9755 <74667763+Adhyatma9755@users.noreply.github.com>
2021-04-06 20:26:21 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,24 @@
+class Ball {
+    constructor(x,y,radius) {
+      var options = {
+          isStatic: false,
+          restitution:0.3
+      }
+      this.body = Bodies.circle(x,y,radius,options);
+      this.radius = radius ;
+      World.add(world, this.body);
+    }
+    display(){
+      var position =this.body.position;
+      var angle = this.body.angle;
+      push();
+      rotate(angle);
+      translate(position.x,position.y);
+      elpsMode(CENTER);
+      circle(0,0, this.radius);
+      pop();
+    }
+  
+    
+    };
+    
